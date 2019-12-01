bb4ef63d68293dbb2bad80d11300aacdebaacdcd
geoklon1997 <georgeklonos7@gmail.com>
2019-12-01 13:12:31 +0000

    Added log out
    Added log out and welcome text

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,39 @@
-if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
-const express = require('express');
-const app = express();
-const volleyball = require('volleyball');
 
+function callAPI() {
 
-app.set('port', (process.env.PORT || 5000));
-
-const fetchData = require('./api/routes/fetch')
-
-app.use(volleyball);
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.use('/fetch', fetchData);
-
-app.use((req, res, next) => {
-    const error = new Error('Not found')
-    error.status = 404
-    next(error)
-  })
+  var x = document.getElementById("address-finder");
+  console.log(x.value)
+  if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
+  const express = require('express');
+  const app = express();
+  const volleyball = require('volleyball');
   
-  app.use((error, req, res, next) => {
-    res.status(error.status || 500)
-    res.json({
-      error: {
-        message: error.message
-      }
+  
+  app.set('port', (process.env.PORT || 5000));
+  
+  const fetchData = require('./api/routes/fetch')
+  
+  app.use(volleyball);
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+  
+  app.use('/fetch', fetchData);
+  console.log('data returned');
+  console.log(fetchData)
+  app.use((req, res, next) => {
+      const error = new Error('Not found')
+      error.status = 404
+      next(error)
     })
-  })
-  
-  app.listen(app.get('port'), console.log(`Server is up and running on port: ${app.get('port')}`));
+    
+    app.use((error, req, res, next) => {
+      res.status(error.status || 500)
+      res.json({
+        error: {
+          message: error.message
+        }
+      })
+    })
+    
+    app.listen(app.get('port'), console.log(`Server is up and running on port: ${app.get('port')}`));
+}
