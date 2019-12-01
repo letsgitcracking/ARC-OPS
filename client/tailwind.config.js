abe8eaa6f37f50b215bc7a4c0e98e8cddde3d835
letsgitcracking <digitalmailsecure@gmail.com>
2019-12-01 00:16:08 +0000

    First commit.

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'custom-blue': '#1B3864',
+        'light-blue': '#03ADD4',
+        'font-blue': '#03AEE6',
+        'font-gray': '#465866',
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
